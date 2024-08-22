9f03befef72b5c709de23694ef90865c9949625a
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-21 21:04:19 -0500

    user-controller

--- src/main/java/com/riwi/JPA/Controller/implementations/UserControllerImplementation.java
+++ src/main/java/com/riwi/JPA/Controller/implementations/UserControllerImplementation.java
@@ -1,4 +1,25 @@
 package com.riwi.JPA.Controller.implementations;
 
-public class UserControllerImplementation {
+import com.riwi.JPA.Controller.interfaces.UserController;
+import com.riwi.JPA.entities.UserEntity;
+import com.riwi.JPA.services.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+@RequestBody();
+public class UserControllerImplementation implements UserController {
+
+    @Autowired
+    UserService userService;
+    @Override
+    @PostMapping("/create")
+    public ResponseEntity<UserEntity> create(@RequestBody  UserEntity userEntity) {
+        return ResponseEntity.ok(userService.save(userEntity));
+    }
 }
